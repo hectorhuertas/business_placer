50ffc0ca56b16bbc1f436f408fbd7242e4a7cd97
Hector Huertas <hectorhuertas@gmail.com>
2016-02-27 17:05:16 -0700

    Setup bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,35 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery
-//= require materialize-sprockets
+//= require bootstrap
 //= require_tree .
-// var loaded = function(){
-//
-//   $(document).ready(function() {
-//     $('.modal-trigger').leanModal();
-//     $(".button-collapse").sideNav();
-//
-//     var $chips = $('.chip');
-//
-//     $('#name_search_name').keyup('change', function () {
-//     var currentName = this.value;
-//     $chips.each(function (index, chip) {
-//       $chip = $(chip);
-//       if ($chip.data('name').includes(currentName)) {
-//         $chip.show();
-//       } else {
-//         $chip.hide();
-//       }
-//     });
-//   });
-//   });
-// }
-
-$(document).ready(function() {
-    $('select').material_select();
-});
-
-// $(document).on("page:load ready", loaded);
-
-// $.getJSON('/').then(whaever)
